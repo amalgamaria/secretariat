83efbf3f9cf4f2caee71de206e02da1d1f585fba
amalgamaria <meadonay@ncsu.edu>
2019-10-19 14:52:17 -0400

    Standardize python notation

--- docs/conf.py
+++ docs/conf.py
@@ -4,18 +4,18 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = ‘secretariat’
-copyright = ‘2019, Maria E. Adonay’
-author = ‘Maria E. Adonay’
-release = '0.1.0'
+project = "secretariat"
+copyright = "2019, Maria E. Adonay"
+author = "Maria E. Adonay"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 
-master_doc = 'index'
-source_suffix = ['.rst']
+master_doc = "index"
+source_suffix = [".rst"]
 
 
-extensions = ['sphinx_rtd_theme',]
+extensions = ["sphinx_rtd_theme",]
 
 # -- Options for HTML output -------------------------------------------------
 
