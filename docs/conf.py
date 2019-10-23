4118a7601d472a3645e0e4f6dce448f585065365
amalgamaria <meadonay@ncsu.edu>
2019-10-23 12:44:29 -0400

    Fix TOC -v3

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
 import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = "secretariat"
-copyright = "2019, Maria E. Adonay"
-author = "Maria E. Adonay"
-release = "0.1.0"
+project = 'secretariat'
+copyright = '2019, Maria E. Adonay'
+author = 'Maria E. Adonay'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
-master_doc = "index"
-source_suffix = [".rst", ]
-extensions = ["sphinx_rtd_theme", ]
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+master_doc = 'index'
+source_suffix = ['.rst', ]
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx_rtd_theme', 
+]
 
 from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
@@ -23,6 +32,8 @@ primary_domain = 'php'
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
+htmlhelp_basename = 'secretariatdoc'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_theme_path = ['_themes', ]
 
